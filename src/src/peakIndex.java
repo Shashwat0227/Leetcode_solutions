fe82d94c000a7063be30336f25b2326e84c3e671
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-08-30 22:08:20 +0530

    Peak index in the mountain array , the bruteforce way

---
+++ src/src/peakIndex.java
@@ -0,0 +1,11 @@
+public class peakIndex {
+    public int peakIndexInMountainArray(int[] arr) {
+        int temp =0;
+        for(int i =1 ; i< arr.length -1;i++){
+            if (arr[i-1]<arr[i]&&arr[i+1]<arr[i]){
+                temp =i;
+            }
+        }
+        return temp;
+    }
+}
