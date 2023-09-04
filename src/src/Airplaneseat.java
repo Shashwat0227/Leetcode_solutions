f9b2016530d3b6429bd028a70856b8dde03ca93b
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-09-04 21:51:57 +0530

    airplane seat ez

---
+++ src/src/Airplaneseat.java
@@ -0,0 +1,10 @@
+public class Airplaneseat {
+    public double nthPersonGetsNthSeat(int n) {
+        if(n==1){
+            return 1 ;
+
+        }else {
+            return 0.50;
+        }
+    }
+}
