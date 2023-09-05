a6db8de764d7c2b51101b4e38316d6b87c37e315
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-09-05 22:35:32 +0530

    Two sum II done binary search approach

---
+++ src/src/TwoSumII.java
@@ -0,0 +1,19 @@
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; ++i) {
+            int low = i + 1;
+            int high = numbers.length - 1;
+            while (low <= high) {
+                int mid = (high - low) / 2 + low;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
