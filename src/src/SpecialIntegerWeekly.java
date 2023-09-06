823704d3d2aa2e8ae38d0b5bfd5c72295c75e9d5
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-09-06 22:07:48 +0530

    trash1

---
+++ src/src/SpecialIntegerWeekly.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class SpecialIntegerWeekly {
+    public int minimumOperations(String num) {
+        int count =0;
+        int n = num.length();
+        int []arr = new int [n];
+        for(int i =0 ; i<arr.length ;i++){
+            arr[i]=num.charAt(i);
+        }
+        if(num.charAt(0)!='0'|| num.charAt(0)!='5'){
+        }
+    }
+}
