b0e80b4f7552eb9db924ead1e21993480101f4e7
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-09-14 22:35:32 +0530

    Longest Substring without Repeating

---
+++ src/src/intersectionoftwoarrays.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class intersectionoftwoarrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> numbers = new HashSet<>();
+        for (int n : nums1) { numbers.add(n); }
+        int[] res = new int[numbers.size()];
+        int cursor = 0;
+        for (int n : nums2) {
+            if (numbers.remove(n)) {
+                res[cursor++] = n;
+            }
+        }
+        return Arrays.copyOfRange(res,0,cursor);
+    }
+}
