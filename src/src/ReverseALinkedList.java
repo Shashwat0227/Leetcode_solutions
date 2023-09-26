3882fa25c34e4aa06afafe898d9b7059e209b14b
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-09-26 22:17:47 +0530

    Reverse A linkedlist (revision)

---
+++ src/src/ReverseALinkedList.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReverseALinkedList {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = null;
+        while (head!=null){
+            ListNode next = head.next;
+            head.next=newHead;
+            newHead=head;
+            head = next;
+
+        }
+        return newHead;
+    }
+    }
+
