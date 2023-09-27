b4f188d0e5dee47cb730a95d261ae170167a97f6
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-09-27 22:21:09 +0530

    Fibonacci using recursion(revision)

---
+++ src/src/ClimbingStairs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
+    }
+
+    private int climbStairs(int n, Map<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
+        }
+        return memo.get(n);
+    }
+}
