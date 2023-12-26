b063e52d4e32be053cca8114ea17ee3b489c511c
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-12-26 23:15:31 +0530

    Linked List !

---
+++ src/src/DeleteNthNode.java
@@ -0,0 +1,35 @@
+public class DeleteNthNode {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+//    } class Solution {
+//            public ListNode removeNthFromEnd(ListNode head, int n) {
+//                ListNode t1 = head;
+//                ListNode t2 = head;
+//                for(int i =0 ;i<n; i++)
+//                    t2 = t2.next;
+//                if(t2==null){
+//                    return head.next;
+//                }
+//                while ( t2.next!=null){
+//
+//                    t1=t1.next;
+//                    t2= t2.next;
+//
+//                }
+//                t1.next = t1.next.next;
+//                return head;
+
+//            }
+//
+    public static void main(String[] args) {
+        System.out.println("leetcode linkedlist class , i just am too tired to put it up for intellij");
+    }
+}
