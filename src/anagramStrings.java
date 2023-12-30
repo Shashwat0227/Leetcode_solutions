2de88be6f63f20681680927f5e7a9ea15c664949
shashwat chaturvedi <shashwatchaturvedig@gmail.com>
2023-12-30 11:48:03 +0530

    anagram strings

---
+++ src/anagramStrings.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class anagramStrings {
+    int map[]=new int[26];
+        for(char x : s.toCharArray()){
+        map[x-'a']++;
+    }
+        for(char x : t.toCharArray()){
+        map[x-'a']--;
+    }
+        for(int i:map){
+        if (i!=0){
+            return false;
+        }
+    }
+        return true;
+}
+}
